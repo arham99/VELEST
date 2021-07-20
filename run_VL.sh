ac4b1aa575b21dd9655dcf4c5896ca3aa63ebe22
arham zakki edelo <azedelo1999@gmail.com>
2021-07-20 12:03:03 +0700

    Update run_VL.sh

--- run_VL.sh
+++ run_VL.sh
@@ -1,10 +1,12 @@
 echo "============"
 echo "Script to Collect Stattistic result From NonLinLoc - Global mode"
 echo "Developed by Arham Zakki Edelo TG-ITS"
-echo "Email: azedelo1999@gmail.com"
+echo "Email_1: azedelo1999@gmail.com"
+echo "Email_2: edelo.arham@gmail.com"
+
 echo
 echo "IMPORTANT"
-echo "   1. Taruh SH dan  Script stat_event.py pada directory yang sama dengan data yang akan di collect"
+echo "   1. Taruh script run_VL.SH dan Script COLLECT_VELEST.py ini pada directory yang sama dengan data yang akan di collect"
 echo "   2. Harus terinstall python3 dengan library pandas "
 echo
 
@@ -12,3 +14,8 @@ echo
 
 echo
 python3 ./VELEST_COLLLECT.py
+
+echo "!!!!NOTE!!!!!!"
+echo "  1. SETELAH INI WAJIB GANTI KODE STASIUN PADA FILE OUTPUT .CNV DENGAN INTEGER (ANGKA) AGAR DAPAT DIBACA OLEH VELEST"
+echo "  2. DISARANKAN MENGGUNAKAN TEXT EDITOR SEPERTI KATE, SUBLIME TEXT, NOTEPAD++, ATOM, DLL"
+echo "  3. PROSES FIND & REPLACE DENGAN TEXT EDITOR HARUS MEMPERTAHANKAN FORMAT AWAL YANG DIHASILKAN TERMASUK SPASINYA   !!!! JANGAN RUBAH FORMAT BAHKAN SPASI SEKLAIPUN!!!"
